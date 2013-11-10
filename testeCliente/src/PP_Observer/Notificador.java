3a47665dc4ff5f4f2ae7754d0fa66617f4ad6eb1
Gilney <bruno_surf07@hotmail.com>
2013-11-10 21:32:15 -0200

    TUDO LIMPO

--- testeCliente/src/PP_Observer/Notificador.java
+++
@@ -1,8 +0,0 @@
-package PP_Observer;
-
-import org.json.JSONObject;
-
-public interface Notificador {
-	public void notificar(JSONObject packet);
-	public void setarNotificavel(Notificavel notificavel);
-}
--- testeCliente/src/PP_Observer/Notificavel.java
+++
@@ -1,7 +0,0 @@
-package PP_Observer;
-
-import org.json.JSONObject;
-
-public interface Notificavel {
-	public void serNotificado(JSONObject packet);
-}
